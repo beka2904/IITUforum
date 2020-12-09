d7ac2c6bb864dc6f1e0365ec566796f65f9927b3
beka2904 <beknazarserikbaev@gmail.com>
2020-12-10 01:32:35 +0600

    Добавил форму Create Post

--- IITUforum/Controllers/AccountController.cs
+++ IITUforum/Controllers/AccountController.cs
@@ -222,7 +222,7 @@ namespace IITUforum.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- IITUforum/Controllers/PostController.cs
+++ IITUforum/Controllers/PostController.cs
@@ -6,6 +6,7 @@ using IITUforum.Data;
 using IITUforum.Data.Models;
 using IITUforum.Models.Post;
 using IITUforum.Models.Reply;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IITUforum.Controllers
@@ -13,9 +14,15 @@ namespace IITUforum.Controllers
     public class PostController : Controller
     {
         private readonly IPost _postService;
-        public PostController(IPost postService)
+        private readonly IForum _forumService;
+
+        private static UserManager<ApplicationUser> _userManager;
+
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
 
         public IActionResult Index(int id)
@@ -37,6 +44,49 @@ namespace IITUforum.Controllers
             };
 
             return View(model);
+        }
+
+        public IActionResult Create(int id)
+        {
+            //forum id
+            var forum = _forumService.GetById(id);
+
+            var model = new NewPostModel
+            {
+                ForumName = forum.Title,
+                ForumId = forum.Id,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+            };
+
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = _userManager.FindByIdAsync(userId).Result;
+            var post = BuildPost(model, user);
+
+            _postService.Add(post).Wait(); 
+            //нужно будет добавить User Rating
+            return RedirectToAction("Index", "Post", new { id = post.Id });
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            var forum = _forumService.GetById(model.ForumId);
+
+            return new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user,
+                Forum = forum
+            };
         }
 
         private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
