981e8e5f9af78f47b22a94ca4fe9dfb9d0500232
beka2904 <beknazarserikbaev@gmail.com>
2020-12-11 00:08:44 +0600

    Исправил "Forum Search bug", начал SearchController

--- IITUforum.Data/IPost.cs
+++ IITUforum.Data/IPost.cs
@@ -10,7 +10,7 @@ namespace IITUforum.Data
     {
         Post GetById(int id);
         IEnumerable<Post> GetAll();
-        IEnumerable<Post> GetFilteredPosts(string searchQuery);
+        IEnumerable<Post> GetFilteredPosts(Forum forum, string searchQuery);
         IEnumerable<Post> GetPostsByForum(int id);
         IEnumerable<Post> GetLatestPosts(int n);
 
